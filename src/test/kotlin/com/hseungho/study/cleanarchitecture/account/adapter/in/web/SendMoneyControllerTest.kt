6dc2587417e665e323a3a3f949209c50abb41c2f
hseungho <hsh47607@gmail.com>
2023-11-11 00:06:55 +0900

    test: SendMoneyController 테스트 코드 작성
    Signed-off-by: hseungho <hsh47607@gmail.com>

--- src/test/kotlin/com/hseungho/study/cleanarchitecture/account/adapter/in/web/SendMoneyControllerTest.kt
+++ src/test/kotlin/com/hseungho/study/cleanarchitecture/account/adapter/in/web/SendMoneyControllerTest.kt
@@ -1,5 +1,45 @@
 package com.hseungho.study.cleanarchitecture.account.adapter.`in`.web
 
-import org.junit.jupiter.api.Assertions.*
+import com.hseungho.study.cleanarchitecture.account.application.port.`in`.SendMoneyCommand
+import com.hseungho.study.cleanarchitecture.account.application.port.`in`.SendMoneyUseCase
+import com.hseungho.study.cleanarchitecture.account.domain.Account
+import com.hseungho.study.cleanarchitecture.account.domain.Money
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
 
-class SendMoneyControllerTest
+@WebMvcTest(SendMoneyController::class)
+class SendMoneyControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockkBean
+    private lateinit var sendMoneyUseCase: SendMoneyUseCase
+
+    @Test
+    fun `testSendMoney`() {
+        // given
+        val command = SendMoneyCommand(
+            sourceAccountId = Account.AccountId(41),
+            targetAccountId = Account.AccountId(42),
+            money = Money.of(500)
+        )
+        every { sendMoneyUseCase.sendMoney(command) } returns true
+        // when
+        // then
+        mockMvc.post("/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}", 41, 42, 500) {
+            header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        }.andExpect {
+            status { isOk() }
+        }
+        verify(exactly = 1) { sendMoneyUseCase.sendMoney(command) }
+    }
+}
